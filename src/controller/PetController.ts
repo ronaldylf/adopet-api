64f68e490001c4a4ad08b9333c3291c67418ede8
Ronaldy <ronaldylf@gmail.com>
2024-08-22 15:22:27 -0300

    added crud functions and geraId

--- src/controller/PetController.ts
+++ src/controller/PetController.ts
@@ -1,11 +1,56 @@
 import { Request, Response } from "express";
+import type TipoPet from "../tipos/TipoPet";
+import EnumEspecie from "../enum/EnumEspecie";
 
-let listaDePets = [];
+let listaDePets: Array<TipoPet> = [];
+// let listaDePets: TipoPet[] = [];
+
+let id = 0;
+function geraId() {
+  id = id + 1;
+  return id;
+}
 
 export default class PetController {
     criaPet(req: Request, res: Response) {
-        const novoPet = req.body;
+        const {adotado, especie, dataDeNascimento, nome} = <TipoPet>req.body;
+        if (!Object.values(EnumEspecie).includes(especie)) {
+            return res.status(400).json({ erro: "Espécie inválida" });
+        }
+
+        const novoPet: TipoPet = {id: geraId(), adotado, especie, dataDeNascimento, nome};
         listaDePets.push(novoPet);
         return res.status(201).json(novoPet);
     }
+
+    listaPets(req: Request, res: Response) {
+        return res.status(200).json(listaDePets);
+    }
+
+    atualizaPets(req: Request, res: Response) {
+        const { id } = req.params;
+        const {adotado, especie, dataDeNascimento, nome} = <TipoPet>req.body;
+        const pet = listaDePets.find((pet) => pet.id===Number(id));
+        if (!pet) {
+            return res.status(404).json({ erro: "Pet não encontrado" });
+        }
+
+        pet.nome = nome;
+        pet.dataDeNascimento = dataDeNascimento;
+        pet.especie = especie;
+        pet.adotado = adotado;
+        return res.status(200).json(pet);
+    }
+
+    deletaPet(req: Request, res: Response) {
+        const { id } = req.params;
+        const pet = listaDePets.find((pet) => pet.id === Number(id));
+        if (!pet) {
+            return res.status(404).json({ mensagem: "Pet não encontrado" });
+        }
+        const indice = listaDePets.indexOf(pet);
+        listaDePets.splice(indice, 1);
+        return res.status(204).json({ mensagem: "Pet deletado com suceso" });
+    }
+
 };
