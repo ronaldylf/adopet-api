b356a924360eb76a60dcbd5acf85489e1573257a
Ronaldy <ronaldylf@gmail.com>
2024-08-22 14:19:46 -0300

    added basic api layers, but with .ts files

---
+++ src/routes/petRouter.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import PetController from '../controller/PetController';
+
+const router = express.Router();
+
+const petController = new PetController();
+
+router.post("/", petController.criaPet);
+
+export default router;
