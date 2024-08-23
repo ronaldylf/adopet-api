7fc994687a3d490b0c12b9cb5e5e21817e6ed0f6
Ronaldy <ronaldylf@gmail.com>
2024-08-23 11:59:56 -0300

    initial database operations with typeorm

---
+++ src/entities/PetEntity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import EnumEspecie from "../enum/EnumEspecie";
+
+@Entity()
+export default class PetEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    especie: EnumEspecie;
+
+    @Column()
+    dataDeNascimento: Date;
+    
+    @Column()
+    adotado: boolean;
+};
