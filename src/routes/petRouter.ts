8a946be7c752ae2346e6019d5927ddea1e01e2f6
Ronaldy <ronaldylf@gmail.com>
2024-08-23 11:57:28 -0300

    routes receiving more context

--- src/routes/petRouter.ts
+++ src/routes/petRouter.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import PetController from '../controller/PetController';
+import PetRepository from '../repositories/PetRepository';
+import { AppDataSource } from '../config/dataSource';
 
 const router = express.Router();
 
-const petController = new PetController();
+const petRepository = new PetRepository(
+    AppDataSource.getRepository("PetEntity")
+);
+const petController = new PetController(petRepository);
 
-router.get("/", petController.listaPets);
-router.post("/", petController.criaPet);
-router.put("/:id", petController.atualizaPets);
-router.delete("/:id", petController.deletaPet);
+router.get("/", (req, res) => { petController.listaPets(req, res) });
+router.post("/", (req, res) => { petController.criaPet(req, res) });
+router.put("/:id", (req, res) => { petController.atualizaPets(req, res) });
+router.delete("/:id", (req, res) => { petController.deletaPet(req, res) });
 
 export default router;
